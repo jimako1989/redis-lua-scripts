978bd6afd6a8641697c08922418dccaa9b4e5d84
jimako <nsplat@gmail.com>
2021-01-02 21:36:17 +0900

    added tests

--- get_script_test.go
+++ get_script_test.go
@@ -64,3 +64,105 @@ func TestScriptTTLAT(t *testing.T) {
 		t.Fatalf("error, expireAt: %v, time+TTL: %v", expireAt, now+TTL)
 	}
 }
+
+func TestScriptHSETEX(t *testing.T) {
+	TTL := 100
+	defer conn.Close()
+
+	script_hsetex, err := GetScript(1, "hsetex.lua")
+	if err != nil {
+		t.Fatalf("error connection to script, %v", err)
+	}
+
+	_, err = script_hsetex.Do(conn, "key", "field", "value", TTL)
+	if err != nil {
+		t.Fatalf("error to hsetex, %v", err)
+	}
+
+	ttl, err := redis.Int(conn.Do("TTL", "key"))
+
+	if ttl != TTL {
+		t.Fatalf("error, actual: %v, expected: %v", ttl, TTL)
+	}
+}
+
+func TestScriptHSETPEX(t *testing.T) {
+	TTL := 2000.0
+	defer conn.Close()
+
+	script_hsetpex, err := GetScript(1, "hsetpex.lua")
+	if err != nil {
+		t.Fatalf("error connection to script, %v", err)
+	}
+
+	_, err = script_hsetpex.Do(conn, "key", "field", "value", TTL)
+	if err != nil {
+		t.Fatalf("error to hsetpex, %v", err)
+	}
+
+	_ttl, err := redis.Int(conn.Do("PTTL", "key"))
+	ttl := float64(_ttl)
+
+	if ttl > TTL || ttl < TTL*0.99 {
+		t.Fatalf("error, actual: %v, expected: %v", ttl, TTL)
+	}
+}
+
+func TestScriptHINCRBYEX(t *testing.T) {
+	TTL := 100
+	defer conn.Close()
+
+	conn.Do("HSET", "key", "field", 1)
+
+	script_hincrbyex, err := GetScript(1, "hincrbyex.lua")
+	if err != nil {
+		t.Fatalf("error connection to script, %v", err)
+	}
+
+	_, err = script_hincrbyex.Do(conn, "key", "field", 99, TTL)
+	if err != nil {
+		t.Fatalf("error to hincrbyex, %v", err)
+	}
+
+	ttl, err := redis.Int(conn.Do("TTL", "key"))
+
+	if ttl != TTL {
+		t.Fatalf("error, actual: %v, expected: %v", ttl, TTL)
+	}
+
+	num, err := redis.Int(conn.Do("HGET", "key", "field"))
+
+	if num != 100 {
+		t.Fatalf("error, actual: %v, expected: 100", num)
+	}
+}
+
+func TestScriptHINCRBYPEX(t *testing.T) {
+	TTL := 2000.0
+	defer conn.Close()
+
+	conn.Do("HSET", "key", "field", 1)
+
+	script_hincrbyex, err := GetScript(1, "hincrbypex.lua")
+	if err != nil {
+		t.Fatalf("error connection to script, %v", err)
+	}
+
+	_, err = script_hincrbyex.Do(conn, "key", "field", 99, TTL)
+	if err != nil {
+		t.Fatalf("error to hincrbypex, %v", err)
+	}
+
+	_ttl, err := redis.Int(conn.Do("PTTL", "key"))
+	ttl := float64(_ttl)
+
+	if ttl > TTL || ttl < TTL*0.99 {
+		t.Fatalf("error, actual: %v, expected: %v", ttl, TTL)
+	}
+
+	num, err := redis.Int(conn.Do("HGET", "key", "field"))
+
+	if num != 100 {
+		t.Fatalf("error, actual: %v, expected: 100", num)
+	}
+}
